bac0b1e45e56456357fb687451296e505ccff351
Jerome <jerome.gir@gmail.com>
2024-11-08 01:57:39 +0100

    Footer version desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,42 +5,46 @@ import instagramIcon from "../assets/icon-instagram.svg"
 
 export default function Footer() {
   return (
-    <div className="flex flex-col items-center justify-center bg-black text-white font-Josefin_Sans ">
-      <h3 className="text-5xl font-bold py-16">loopstudios</h3>
-      <ul className="flex flex-col justify-center items-center gap-4 text-2xl pb-12">
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/">Careers</a>
-        </li>
-        <li>
-          <a href="/">Events</a>
-        </li>
-        <li>
-          <a href="/">Products</a>
-        </li>
-        <li>
-          <a href="/">Support</a>
-        </li>
-      </ul>
-      <ul className="flex justify-center items-center gap-5 pb-4">
-        <a href="/">
-          <img className="size-8" src={facebookIcon} alt="icon facebook" />
-        </a>
-        <a href="/">
-          <img className="size-8" src={twitterIcon} alt="icon facebook" />
-        </a>
-        <a href="/">
-          <img className="size-8" src={pinterestIcon} alt="icon facebook" />
-        </a>
-        <a href="/">
-          <img className="size-8" src={instagramIcon} alt="icon facebook" />
-        </a>
-      </ul>
-      <p className="pt-3 pb-6 text-dark_gray text-xl">
-        © 2021 Loopstudios. All rights reserved.
-      </p>
+    <div className="flex flex-col sm:flex-row sm:justify-around items-center justify-center bg-black text-white font-Josefin_Sans">
+      <div>
+        <h3 className="text-5xl font-bold py-16 sm:py-10">loopstudios</h3>
+        <ul className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-10 text-2xl pb-12">
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Careers</a>
+          </li>
+          <li>
+            <a href="/">Events</a>
+          </li>
+          <li>
+            <a href="/">Products</a>
+          </li>
+          <li>
+            <a href="/">Support</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="flex justify-center sm:justify-end items-center gap-5 pb-4 sm:pb-6">
+          <a href="/">
+            <img className="size-8" src={facebookIcon} alt="icon facebook" />
+          </a>
+          <a href="/">
+            <img className="size-8" src={twitterIcon} alt="icon facebook" />
+          </a>
+          <a href="/">
+            <img className="size-8" src={pinterestIcon} alt="icon facebook" />
+          </a>
+          <a href="/">
+            <img className="size-8" src={instagramIcon} alt="icon facebook" />
+          </a>
+        </ul>
+        <p className="pt-3 pb-6 sm:pb-0 text-dark_gray text-xl sm:text-2xl">
+          © 2021 Loopstudios. All rights reserved.
+        </p>
+      </div>
     </div>
   )
 }
