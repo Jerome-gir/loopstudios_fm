0008ba5bf71b40b5b91028783face8df24993f82
Jerome <jerome.gir@gmail.com>
2024-11-15 21:54:51 +0100

    desktop version ok

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -3,28 +3,28 @@ import CreationsSection from "./CreationsSection"
 
 export default function ContentSection() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center px-12">
-        <div>
+    <div className="md:flex md:justify-center">
+      <div className="flex flex-col items-center justify-center px-12 md:items-start">
+        <div className="md:relative">
           <img
             className="object-cover mb-16"
             src={vrImage}
             alt="personne avec un casque vr"
           />
+          <div className="md:absolute md:bg-white md:w-full md:h-82 md:-bottom-10 md:left-96 md:ml-32 md:pl-20 md:pt-16">
+            <h2 className="text-very_dark_gray text-center md:text-left uppercase text-4xl w-80 mb-12 md:mb-8">
+              the leader in interactive vr
+            </h2>
+            <p className="text-dark_gray font-Josefin_Sans w-96 text-center mb-24 md:text-left">
+              Founded in 2011, Loopstudios has been producing world-class
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
         </div>
-        <div className="md:absolute md:bg-white">
-          <h2 className="text-very_dark_gray text-center md:text-left uppercase text-4xl w-80 mb-12">
-            the leader in interactive vr
-          </h2>
-          <p className="text-dark_gray font-Josefin_Sans w-96 text-center mb-24 md:text-left">
-            Founded in 2011, Loopstudios has been producing world-class virtual
-            reality projects for some of the best companies around the globe.
-            Our award-winning creations have transformed businesses through
-            digital experiences that bind to their brand.
-          </p>
-        </div>
+        <CreationsSection />
       </div>
-      <CreationsSection />
-    </>
+    </div>
   )
 }
--- src/components/CreationsSection.tsx
+++ src/components/CreationsSection.tsx
@@ -18,99 +18,148 @@ import fisheyeDesktop from "../assets/desktop/image-fisheye.jpg"
 
 export default function CreationsSection() {
   return (
-    <div className="md:px-12">
-      <div className="flex flex-col items-center justify-center px-12 md:flex-row md:justify-between">
-        <h2 className="text-very_dark_gray uppercase text-4xl w-80 mb-12 md:mb-28 md:text-4xl">
+    <div className="md:mt-20">
+      <div className="flex flex-col items-center justify-center px-12 md:flex-row md:justify-between md:px-0">
+        <h2 className="text-very_dark_gray uppercase text-4xl w-80 mb-12 md:mb-16 md:text-5xl md:w-96">
           our creations
         </h2>
         <a href="/">
-          <button className="hidden md:block uppercase text-xl border-2 px-12 py-2 font-Alata tracking-widest mb-12 md:mb-28">
+          <button className="hidden md:block uppercase text-xl border-2 px-12 py-2 font-Alata tracking-widest mb-12 md:mb-16 ">
             see all
           </button>
         </a>
       </div>
 
-      <div className="px-6 md:px-12 grid grid-cols-1 gap-6 md:grid-cols-4 md:grid-rows-2 md:mb-44">
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={deepEarth}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={deepEarthDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={nightArcade}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={nightArcadeDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={soccerTeam}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={soccerTeamDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={gridImage}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={gridImageDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={fromAbove}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={fromAboveDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={pocketBorealis}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={pocketBorealisDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover border cursor-pointer"
-          src={curiosity}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={curiosityDesktop}
-          alt="galerie d'images"
-        />
-        <img
-          className="block md:hidden object-cover mb-12 border cursor-pointer"
-          src={fisheye}
-          alt="galerie d'images"
-        />
-        <img
-          className="hidden md:block object-cover border cursor-pointer"
-          src={fisheyeDesktop}
-          alt="galerie d'images"
-        />
+      <div className="md:flex md:justify-center">
+        <div className="px-6 grid grid-cols-1 gap-6 md:grid-cols-4 md:grid-rows-2 md:mb-44 md:px-0">
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={deepEarth}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={deepEarthDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-24 text-white p-8 text-2xl md:text-3xl">
+              deep earth
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={nightArcade}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={nightArcadeDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-24 text-white p-8 text-2xl md:text-3xl">
+              night arcade
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={soccerTeam}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={soccerTeamDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-full text-white p-8 text-2xl md:text-3xl">
+              <span className="flex flex-col">soccer</span>team vr
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={gridImage}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={gridImageDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-24 text-white p-8 text-2xl md:text-3xl">
+              the grid
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={fromAbove}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={fromAboveDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute  flex-col bottom-0 left-0 uppercase font-thin w-full text-white p-8 text-2xl md:text-4xl">
+              <span className="flex flex-col">from up</span>above vr
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={pocketBorealis}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={pocketBorealisDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-24 text-white p-8 text-2xl md:text-3xl">
+              pocket borealis
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover border cursor-pointer"
+              src={curiosity}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={curiosityDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute bottom-0 left-0 uppercase font-thin w-24 text-white p-8 text-2xl md:text-3xl">
+              the curiosity
+            </div>
+          </div>
+
+          <div className="relative">
+            <img
+              className="block md:hidden object-cover mb-12 border cursor-pointer"
+              src={fisheye}
+              alt="galerie d'images"
+            />
+            <img
+              className="hidden md:block object-cover border cursor-pointer"
+              src={fisheyeDesktop}
+              alt="galerie d'images"
+            />
+            <div className="absolute flex-col bottom-0 left-0 uppercase font-thin text-white pl-8 pb-16 text-2xl md:p-8 md:text-4xl">
+              <span className="flex flex-col">make it</span>fisheye
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex justify-center">
         <a href="/">
